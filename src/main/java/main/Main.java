db91f4330f231f571ac78d6d81f6d895a45c655c
ahmad <ahmadalibrahim28@gmail.com>
2019-07-04 11:42:03 +0200

    Bug pom.xml fixed jena version

--- src/main/java/main/Main.java
+++ src/main/java/main/Main.java
@@ -2,6 +2,11 @@ package main;
 
 import com.beust.jcommander.JCommander;
 import com.beust.jcommander.Parameter;
+import org.apache.jena.graph.Node;
+import org.apache.jena.graph.Triple;
+import org.apache.jena.riot.lang.PipedRDFIterator;
+import parser.Parser;
+
 import java.io.*;
 
 public class Main {
@@ -16,84 +21,43 @@ public class Main {
 
     public void run() throws Exception {
         try {
-            if(owlFile!=null && datasetFile!=null && outputFile!=null ) {
+            if (owlFile != null && datasetFile != null && outputFile != null)
+            {
                 BufferedWriter writer = new BufferedWriter(new FileWriter(outputFile));
-                BufferedReader readerDataset = new BufferedReader(new FileReader(datasetFile));
-                String lineDataset;
-                while ((lineDataset = readerDataset.readLine()) != null) {
-                    String[] tripleDataset = lineDataset.split(" ");
-                    String subjectDataset = tripleDataset[0];
-                    String predicateDataset = tripleDataset[1];
-                    String objectDataset = tripleDataset[2];
+                PipedRDFIterator<Triple> iteratorDataset = Parser.parse("/home/ahmad/IdeaProjects/SubstitutingAlignments/dataset.nt");
+                while (iteratorDataset.hasNext()) {
+                    Triple tripleDataset = iteratorDataset.next();
+                    Node subjectDataset = tripleDataset.getSubject();
+                    Node objectDataset = tripleDataset.getObject();
+                    Node predicateDataset = tripleDataset.getPredicate();
                     boolean found = false;
-                    BufferedReader readerOwl = new BufferedReader(new FileReader(owlFile));
-                    String lineOwl;
-                    while ((lineOwl = readerOwl.readLine()) != null) {
-                        String[] tripleOwl = lineOwl.split(" ");
-                        String subjectOwl = tripleOwl[0];
-                        String objectOwl = tripleOwl[2];
-
+                    PipedRDFIterator<Triple> iteratorOwl = Parser.parse(owlFile);
+                    while (iteratorOwl.hasNext()) {
+                        Triple tripleOwl = iteratorOwl.next();
+                        Node subjectOwl = tripleOwl.getSubject();
+                        Node objectOwl = tripleOwl.getObject();
                         if (subjectDataset.equals(subjectOwl)) {
-                            writer.write(objectOwl + " " + predicateDataset + " " + objectDataset + " .\n");
+                            writer.write("<" + objectOwl + "> <" + predicateDataset + "> <" + objectDataset + "> .\n");
                             found = true;
                             break;
                         } else if (subjectDataset.equals(objectOwl)) {
-                            writer.write(subjectOwl + " " + predicateDataset + " " + objectDataset + " .\n");
+                            writer.write("<" + subjectOwl + "> <" + predicateDataset + "> <" + objectDataset + "> .\n");
                             found = true;
                             break;
                         }
                     }
-                    readerOwl.close();
                     if (found == false) {
-                        writer.write(subjectDataset + " " + predicateDataset + " " + objectDataset + " .\n");
+                        writer.write("<" + subjectDataset + "> <" + predicateDataset + "> <" + objectDataset + "> .\n");
                     }
                 }
-                readerDataset.close();
+                System.out.println("Finished");
                 writer.close();
+                System.exit(0);
             }
-            else
-            {
-                System.out.println("Error in parameters");
+            else {
+                System.out.println("Wrong parameters");
             }
-
-//            if(owlFile!=null && datasetFile!=null && outputFile!=null) {
-//                BufferedWriter writer = new BufferedWriter(new FileWriter(outputFile));
-//                PipedRDFIterator<Triple> iteratorDataset = Parser.parse("/home/ahmad/IdeaProjects/SubstitutingAlignments/dataset.nt");
-//                System.out.println(iteratorDataset.hasNext());
-//                while (iteratorDataset.hasNext()) {
-//                    Triple tripleDataset = iteratorDataset.next();
-//                    Node subjectDataset = tripleDataset.getSubject();
-//                    Node objectDataset = tripleDataset.getObject();
-//                    Node predicateDataset = tripleDataset.getPredicate();
-//                    boolean found = false;
-//                    PipedRDFIterator<Triple> iteratorOwl = Parser.parse(owlFile);
-//                    while (iteratorOwl.hasNext()) {
-//                        Triple tripleOwl = iteratorOwl.next();
-//                        Node subjectOwl = tripleOwl.getSubject();
-//                        Node objectOwl = tripleOwl.getObject();
-//                        if (subjectDataset.equals(subjectOwl)) {
-//                            writer.write("<" + objectOwl + "> <" + predicateDataset + "> <" + objectDataset + "> .\n");
-//                            found = true;
-//                            break;
-//                        } else if (subjectDataset.equals(objectOwl)) {
-//                            writer.write("<" + subjectOwl + "> <" + predicateDataset + "> <" + objectDataset + "> .\n");
-//                            found = true;
-//                            break;
-//                        }
-//                    }
-//                    if (found == false) {
-//                        writer.write("<" + subjectDataset + "> <" + predicateDataset + "> <" + objectDataset + "> .\n");
-//                    }
-//                }
-//                System.out.println("Finished");
-//                writer.close();
-
-//                System.exit(0);
-//            }
-//            else {
-//                System.out.println("Wrong parameters");
-//            }
-            }
+        }
         catch(Exception e)
         {
             e.printStackTrace();
--- src/main/java/parser/Parser.java
+++ src/main/java/parser/Parser.java
@@ -1,35 +1,32 @@
 package parser;
+
 import org.apache.jena.graph.Triple;
-import org.apache.jena.riot.Lang;
 import org.apache.jena.riot.RDFDataMgr;
 import org.apache.jena.riot.lang.PipedRDFIterator;
 import org.apache.jena.riot.lang.PipedRDFStream;
 import org.apache.jena.riot.lang.PipedTriplesStream;
+
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 
-public class Parser
-{
-    private static ExecutorService executor = Executors.newSingleThreadExecutor();
-
-    public static void parse (final String dump)
+public class Parser {
+    public static PipedRDFIterator<Triple> parse(String dump)
     {
         PipedRDFIterator<Triple> iter = new PipedRDFIterator<Triple>();
-
-        System.out.println(dump);
+        final String d = dump;
         final PipedRDFStream<Triple> inputStream = new PipedTriplesStream(iter);
-
-        Runnable parser = new Runnable()
-        {
+        // PipedRDFStream and PipedRDFIterator need to be on different threads
+        ExecutorService executor = Executors.newSingleThreadExecutor();
+        // Create a runnable for our parser thread
+        Runnable parser = new Runnable() {
             @Override
-            public void run()
-            {
-                System.out.println(iter.next());
-//                RDFDataMgr.parse(inputStream,dump);
+            public void run() {
+                // Call the parsing process.
+                RDFDataMgr.parse(inputStream, d);
             }
         };
+        // Start the parser on another thread
         executor.submit(parser);
-//        System.out.println(iter.hasNext());
-//        return iter;
+        return iter;
     }
 }
